c03ae202da9580a812adf3dc694e0f2d0829db09
rafa <rafa-spdf@hotmail.com>
2026-08-07 10:41:59 -0300

    fiz toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "@/components/ui/sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
@@ -150,6 +151,7 @@ function App() {
           <AuthenticatedApp />
         </Router>
         <Toaster />
+        <SonnerToaster richColors position="top-center" />
       </QueryClientProvider>
     </AuthProvider>
   )
